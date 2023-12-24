f2e987090e8824c8ea0ae24b587d2cea2fb3bf77
fashaap <bmpasha23@gmail.com>
2023-12-24 13:18:00 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+
+import { useState } from "react";
+import axios from "axios"
+function App() {
+  const [postImage, setPostImage] = useState({ myFile: "" });
+
+  const url = "https://localhost:3000/uploads"
+
+  const createPost = async (newImage) => {
+    try {
+      await axios.post(url, newImage)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createPost(postImage);
+    
+  };
+
+  const handleFileUpload = async (e) => {
+    const file = e.target.files[0];
+    const base64 = await convertToBase64(file);
+    setPostImage({ myFile: base64 });
+    //setPostImage({ ...postImage, myFile: base64 });
+    //console.log({myFile : base64});
+  };
+
+  function convertToBase64(file) {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  }
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="file"
+            label="image"
+            name="myFile"
+            id="file-upload"
+            accept=".jpg, .jpeg, .png"
+            onChange={(e) => handleFileUpload(e)}
+          />
+        </form>
+        <div>
+          <img src={postImage.myFile} alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
